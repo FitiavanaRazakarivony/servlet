43461ba0e440ebdee03c57e51f3ee98b1d8d4349
FitiavanaRazakarivony <120584452+FitiavanaRazakarivony@users.noreply.github.com>
2023-04-04 08:53:31 +0200

    Add files via upload

---
+++ serv/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example.demo1;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+    }
+
+    public void destroy() {
+
+    }
+}
